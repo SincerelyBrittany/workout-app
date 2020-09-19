e8119c1bdd0524e27a14b0e964f7ef2c9e098bc8
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-19 11:07:54 -0400

    adds page mamanger and event listeners

--- workoutAppFrontend/src/components/app.js
+++ workoutAppFrontend/src/components/app.js
@@ -2,4 +2,10 @@ class App{
     constructor(){
         this.initBindingsAndEventListeners()
     }
+    initBindingsAndEventListeners(){
+        this.container = document.querySelector('#app-container')
+        this.alertContainer = document.querySelector('#alert-container')
+        this.navbarContainer = document.querySelector('#navbar-container')
+        this.pageContainer = document.querySelector('#page-container')
+    }
 }
