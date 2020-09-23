bfaeef081a5005c3515838a933df62241833e6dd
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-23 09:42:00 -0400

    adds fetch and render page resources

---
+++ workoutAppFrontend/src/pageManagers/pageManager.js
@@ -0,0 +1,42 @@
+class PageManager{
+
+    constructor(container, adapter){
+        this.container = container 
+        this.adapter = new SignupAdapter(adapter)
+    }
+
+   fetchAndRenderPageResources(){
+       return null
+   }
+
+       render(){
+        this.container.innerHTML = this.staticHTML
+        this.initBindingsAndEventListeners()
+        this.fetchAndRenderPageResources()
+    }
+
+
+    // constructor(container){
+    //     this.container = container    
+    // }
+
+    // fetchAndRenderPageResources(){
+    //     return null
+    // }
+
+    // handleError(err){
+    //     if(err.type === "Authorizaiton Error"){
+    //         this.handleAlert(err.msg)
+    //         this.redirect('welcome')
+    //     }else{
+    //         this.handleAlert(err)
+    //     }
+    // }
+
+    // render(){
+    //     this.container.innerHTML = this.staticHTML
+    //     this.initBindingsAndEventListeners()
+    //     this.fetchAndRenderPageResources()
+    // }
+
+}
