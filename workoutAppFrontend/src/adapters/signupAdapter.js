cc7177e452734b03075fe81755eeaa1a9278ed2f
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-23 09:40:44 -0400

    adds adapter to make fetch request to server

---
+++ workoutAppFrontend/src/adapters/signupAdapter.js
@@ -0,0 +1,31 @@
+class SignupAdapter{
+
+    constructor(baseAdapter){
+        this.baseAdapter = baseAdapter
+        this.baseURL = this.baseAdapter.baseURL
+    }
+
+    get token(){
+        return this.baseAdapter.token
+    }
+
+    get headers(){
+        return this.baseAdapter.headers
+    }
+
+    // checkStatus(res){
+    //     this.baseAdapter.checkStatus(res)
+    // }
+
+    async signup(params){
+        const res = await fetch(`${this.baseURL}/signup`, {
+            method: 'POST',
+            headers: this.headers,
+            body: JSON.stringify(params)
+        })
+        await this.baseAdapter.checkStatus(res)
+        this.baseAdapter.token = res.headers.get('authorization').split(' ')[1]
+        return await res.json()
+    }
+
+}
